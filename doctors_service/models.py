5503fc86fb4233d7f30afbbc8dc9f8b6fa87b704
Alla Kuksa <alla.kuksa92@gmail.com>
2024-04-10 19:27:45 +0300

    add AUTH_USER_MODEL in settings.py

--- doctors_service/models.py
+++ doctors_service/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
@@ -10,3 +11,45 @@ class DoctorSpecialty(models.Model):
     def __str__(self):
         return self.specialty
 
+
+class Doctor(AbstractUser):
+    licence_number = models.IntegerField(max_length=8, unique=True, blank=False)
+    city = models.CharField(max_length=255)
+    hospital = models.CharField(max_length=255)
+    specialty = models.ManyToManyField(
+        DoctorSpecialty,
+        on_delete=models.CASCADE,
+        related_name="doctors"
+    )
+
+    class Meta:
+        ordering = ("hospital", "city",)
+
+    def __str__(self):
+        return (f"{self.first_name} {self.last_name} "
+                f"with specialty {self.specialty}  - "
+                f"city {self.city} in {self.hospital}")
+
+
+class DoctorSchedule(models.Model):
+
+    TIMESLOT_LIST = (
+        (0, "09:00 – 10:00"),
+        (1, "10:00 – 11:00"),
+        (2, "11:00 – 12:00"),
+        (3, "12:00 – 13:00"),
+        (4, "14:00 – 15:00"),
+        (5, "15:00 – 16:00"),
+        (6, "16:00 – 17:00"),
+        (7, "17:00 – 18:00"),
+    )
+    doctor = models.ForeignKey(
+        Doctor,
+        on_delete=models.CASCADE,
+        related_name="doctor_schedule",
+    )
+    date = models.DateField(help_text="YYYY-MM-DD")
+    timeslot = models.IntegerField(choices=TIMESLOT_LIST)
+
+    class Meta:
+        unique_together = ("doctor", "date", "timeslot", )
