e0f3bcaad281daf83ced7fb0f67bcfd2c0632037
Alla Kuksa <alla.kuksa92@gmail.com>
2024-05-06 19:16:37 +0300

    add tests for doctors view

--- tests/test_views_doctor_specialty.py
+++ tests/test_views_doctor_specialty.py
@@ -1,9 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
-from doctors_service.models import (
-    DoctorSpecialty,
-    Doctor)
+from doctors_service.models import DoctorSpecialty
 
 DOCTOR_SPECIALTY_URL = reverse("doctors_service:specialties-list")
 DOCTOR_SPECIALTY_DETAIL_URL = reverse(
